a9b307bcbd49c4b22f70cfeec12562f0231e9bf8
All in Jest <jessemart07@gmail.com>
2020-09-13 19:59:20 +0200

    -update tabs to work with conditional rendering

--- src/containers/BudgetBuddy/BudgetBuddy.js
+++ src/containers/BudgetBuddy/BudgetBuddy.js
@@ -3,6 +3,7 @@ import Budget from '../../components/Budget/Budget';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import TabPanel from '../../utils/TabPanel';
 import Transactions from '../../components/Transactions/Transactions';
 import { Container } from '@material-ui/core';
 import classes from './BudgetBuddy.module.css';
@@ -11,12 +12,25 @@ import classes from './BudgetBuddy.module.css';
 class BudgetBuddy extends Component {
     state = { 
         value: 0
+        
     }
-
+    
     handleChange = (event, newValue) => {
         this.setState({value: newValue});
     };
+
+    a11yProps = (index) => {
+        return {
+          id: `simple-tab-${index}`,
+          'aria-controls': `simple-tabpanel-${index}`,
+        };
+      }
     render(){
+        let body = <Budget></Budget>;
+        if(this.state.value == 1){
+            body = <Transactions></Transactions>;
+        }
+        
         return (
             <div className={classes.BudgetBuddy}>
                 <Paper square>
@@ -24,14 +38,19 @@ class BudgetBuddy extends Component {
                     value={this.state.value}
                     indicatorColor="primary"
                     textColor="secondary"
-                    aria-label="disabled tabs example"
+                    aria-label="simple tabs example"
                     onChange={this.handleChange}
                     >
-                        <Tab label="Budget" value={this.state.value} index={0}/>
-                        <Tab label="Transactions" value={this.state.value} index={1}/>
+                        <Tab label="Budget"  {...this.a11yProps(0)}/>
+                        <Tab label="Transactions" {...this.a11yProps(1)}/>
                     </Tabs>
                 </Paper>
-                <Budget></Budget>
+                <TabPanel value={this.state.value} index={0}>
+                    <Budget></Budget>
+                </TabPanel>
+                <TabPanel value={this.state.value} index={1}>
+                    <Transactions></Transactions>
+                </TabPanel>
             </div>
         )
     }
